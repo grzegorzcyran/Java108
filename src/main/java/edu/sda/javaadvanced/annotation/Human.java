811a62cc9354bef1e5b9163286cd40e20446a86e
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2022-04-10 09:27:10 +0200

    Day 5 - annotations

---
+++ src/main/java/edu/sda/javaadvanced/annotation/Human.java
@@ -0,0 +1,6 @@
+package edu.sda.javaadvanced.annotation;
+
+@HumanInfo(name = "SampleHuman", noOfChildren = 3)
+public class Human {
+
+}
---
+++ src/main/java/edu/sda/javaadvanced/annotation/HumanInfo.java
@@ -0,0 +1,6 @@
+package edu.sda.javaadvanced.annotation;
+
+public @interface HumanInfo {
+    String name();
+    int noOfChildren();
+}
