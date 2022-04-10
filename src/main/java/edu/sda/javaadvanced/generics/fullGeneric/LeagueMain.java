9a0ce37a5bb623e9937e5564eba2b0ccfdf6756d
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2022-04-10 10:29:45 +0200

    Day 5 - generics - generic approach

---
+++ src/main/java/edu/sda/javaadvanced/generics/fullGeneric/LeagueMain.java
@@ -0,0 +1,33 @@
+package edu.sda.javaadvanced.generics.fullGeneric;
+
+import edu.sda.javaadvanced.generics.FootballTeam;
+import edu.sda.javaadvanced.generics.Team;
+import edu.sda.javaadvanced.generics.TeamLevel;
+import edu.sda.javaadvanced.generics.VolleyballTeam;
+
+public class LeagueMain {
+    public static void main(String[] args) {
+        League<Team> league = new League("Primiera Division");
+
+        Team team1 = new FootballTeam("FC Barcelona", TeamLevel.PROFESSIONAL);
+        Team team2 = new FootballTeam("Real Madrid CF", TeamLevel.PROFESSIONAL);
+        Team team3 = new FootballTeam("Atletico de Madrid", TeamLevel.PROFESSIONAL);
+        Team team4 = new FootballTeam("Valencia CF", TeamLevel.PROFESSIONAL);
+        team1.setPoins(30);
+        team2.setPoins(25);
+        team3.setPoins(33);
+        team4.setPoins(28);
+
+        league.addTeam(team1);
+        league.addTeam(team2);
+        league.addTeam(team3);
+        league.addTeam(team4);
+        System.out.println("===========");
+        league.printTable();
+        System.out.println("===========");
+        Team team5 = new VolleyballTeam("Asseco Resovia", TeamLevel.PROFESSIONAL);
+        league.addTeam(team5);
+        team5.setPoins(27);
+        league.printTable();
+    }
+}
